9f2bf52a50ba495847811a97be75e2062e55fd58
webklex <info@webklex.com>
2026-02-22 12:13:42 +0100

    build consts support added

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
-declare const __SINKER_PLUGIN_VERSION__: string;
-export const PLUGIN_VERSION =
-    typeof __SINKER_PLUGIN_VERSION__ !== 'undefined'
-        ? __SINKER_PLUGIN_VERSION__
-        : (process.env.npm_package_version ?? '0.0.0');
+import * as generated from './consts.generated';
+
+export const SINKER_VERSION =
+    generated.SINKER_VERSION ?? process.env.npm_package_version ?? '0.0.0';
+
+export const CONFIG_FILE = generated.CONFIG_FILE ?? 'sinker.config.js';
